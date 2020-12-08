8174420afb2c7de6e02f632bc811ca3e4e803e8e
hannibal <jxlaa11@qq.com>
2020-12-08 13:04:49 +0800

    fix algorithm and add test data

--- src/gamerummy.cpp
+++ src/gamerummy.cpp
@@ -8,7 +8,6 @@
 #include <assert.h>
 using std::cout;
 using std::endl;
-using std::for_each;
 using std::map;
 using std::shared_ptr;
 using std::string;
@@ -36,158 +35,16 @@ void CardGroup::removeCard(const PCard &card)
 
 void CardGroup::removeGroup(const CardGroup &rhs)
 {
-    for_each(rhs.cardlist_.begin(), rhs.cardlist_.end(),
-             [this](const PCard &card) {
-                 removeCard(card);
-             });
+    std::for_each(rhs.cardlist_.begin(), rhs.cardlist_.end(),
+                  [this](const PCard &card) {
+                      removeCard(card);
+                  });
 }
 
 void CardGroup::reset()
 {
     id_ = genId();
     cardlist_.clear();
-}
-
-
-int Mgr::match(const map<int, PCard> &hand, vector<CardGroup> &grouplist)
-{
-    grouplist.clear();
-    vector<CardGroup> matchlist;
-    vector<CardGroup> candidates;
-    pickRun(hand, matchlist, candidates);
-    return 0;
-}
-
-int Mgr::pickRun(const map<int, PCard> &hand, vector<CardGroup> &matchlist, vector<CardGroup> &candidates)
-{
-    matchlist.clear();
-    candidates.clear();
-    std::vector<CardGroup> suitlist;
-    suitlist.resize(c_suit);
-    candidates.resize(c_suit);
-    for_each(hand.begin(), hand.end(),
-             [&suitlist, &candidates, this](std::pair<const int, PCard> item) {
-                 if (item.second->rank_ == special_)
-                 {
-                     candidates[e_joker].cardlist_.push_back(item.second);
-                 }
-                 else
-                 {
-                     suitlist[item.second->suit_].cardlist_.push_back(item.second);
-                 }
-             });
-
-    for_each(suitlist.begin(), suitlist.end(),
-             [&matchlist, &candidates](CardGroup &group) {
-                 std::sort(group.cardlist_.begin(), group.cardlist_.end(), [](const PCard &a, const PCard &b) { return a->rank_ > b->rank_; });
-                 pickRunFromGroup(group, matchlist, candidates);
-             });
-
-    cout << "组排后同花组" << endl;
-    for_each(matchlist.begin(), matchlist.end(), Mgr::printCardGroup);
-
-    cout << "组排后候选组" << endl;
-    for_each(candidates.begin(), candidates.end(), Mgr::printCardGroup);
-    return 0;
-}
-
-int Mgr::pickRunFromGroup(const CardGroup &group, vector<CardGroup> &matchlist, vector<CardGroup> &candidates)
-{
-    CardGroup groupCopy(group);
-    int groupSize = groupCopy.cardlist_.size();
-    if (groupSize < c_minimum && groupSize > 0)
-    {
-        int idx = groupCopy.cardlist_[0]->suit_;
-        assert(idx >= 0 && idx <= 4);
-        candidates[idx].cardlist_.insert(candidates[idx].cardlist_.end(),
-                                         groupCopy.cardlist_.begin(), groupCopy.cardlist_.end());
-        return 0;
-    }
-
-    CardGroup unmatched;
-    CardGroup tmpMatched;
-    auto it = groupCopy.cardlist_.begin();
-    tmpMatched.cardlist_.push_back(*it);
-    PCard last = NULL;
-    for (auto it = groupCopy.cardlist_.begin(); it != groupCopy.cardlist_.end(); ++it)
-    {
-        if(last == NULL)
-        {
-            last = *it;
-            continue;
-        }
-        if ((*it)->rank_ == last->rank_)
-        {
-            unmatched.cardlist_.push_back(*it);
-            continue;
-        }
-        else if (last->rank_ - (*it)->rank_ == 1)
-        {
-            tmpMatched.cardlist_.push_back(*it);
-            last = *it;
-        }
-        else
-        {
-            unmatched.cardlist_.insert(unmatched.cardlist_.end(), tmpMatched.cardlist_.begin(), tmpMatched.cardlist_.end());
-            tmpMatched.reset();
-            tmpMatched.cardlist_.push_back(*it);
-            last = *it;
-            continue;
-        }
-        
-        if (tmpMatched.cardlist_.size() == c_minimum - 1)
-        {
-            if ((*(tmpMatched.cardlist_.begin()))->rank_ == 13 && (*(tmpMatched.cardlist_.begin() + 1))->rank_ == 12)
-            {
-                auto ace = std::find_if(groupCopy.cardlist_.begin(), groupCopy.cardlist_.end(), [](const PCard &card) { return card->rank_ == 1; });
-                if (ace != groupCopy.cardlist_.end())
-                {
-                    tmpMatched.cardlist_.insert(tmpMatched.cardlist_.begin(), (*ace));
-                    matchlist.push_back(tmpMatched);
-                    groupCopy.removeGroup(tmpMatched);
-                    tmpMatched.reset();
-                    it = groupCopy.cardlist_.begin();
-                    if (it != groupCopy.cardlist_.end())
-                    {
-                        tmpMatched.cardlist_.push_back(*(it));
-                        last = NULL;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-        else if (tmpMatched.cardlist_.size() == c_minimum)
-        {
-            matchlist.push_back(tmpMatched);
-            groupCopy.removeGroup(tmpMatched);
-            tmpMatched.reset();
-            it = groupCopy.cardlist_.begin();
-            if (it != groupCopy.cardlist_.end())
-            {
-                tmpMatched.cardlist_.push_back(*it);
-            }
-            last = NULL;
-        }
-    }
-
-    groupSize = tmpMatched.cardlist_.size();
-    if (groupSize > 0)
-    {
-        int idx = tmpMatched.cardlist_[0]->suit_;
-        candidates[idx].cardlist_.insert(candidates[idx].cardlist_.end(),
-                                         tmpMatched.cardlist_.begin(), tmpMatched.cardlist_.end());
-    }
-    groupSize = unmatched.cardlist_.size();
-    if (groupSize > 0)
-    {
-        int idx = unmatched.cardlist_[0]->suit_;
-        candidates[idx].cardlist_.insert(candidates[idx].cardlist_.end(),
-                                         unmatched.cardlist_.begin(), unmatched.cardlist_.end());
-    }
-    return 0;
 }
 
 void Mgr::printCardGroup(const CardGroup &group)
@@ -197,11 +54,11 @@ void Mgr::printCardGroup(const CardGroup &group)
         return;
     }
     cout << "分组编号: " << group.id_ << " 卡牌列表" << endl;
-    for_each(group.cardlist_.begin(), group.cardlist_.end(),
-             [](const PCard &card) {
-                 string name = getCardString(*card);
-                 cout << name << endl;
-             });
+    std::for_each(group.cardlist_.begin(), group.cardlist_.end(),
+                  [](const PCard &card) {
+                      string name = getCardString(*card);
+                      cout << name << endl;
+                  });
 }
 
 string Mgr::getCardString(const Card &card)
@@ -262,9 +119,164 @@ string Mgr::getCardString(const Card &card)
     return std::move(name);
 }
 
+int Mgr::match(const map<int, PCard> &hand, vector<CardGroup> &grouplist)
+{
+    grouplist.clear();
+    vector<CardGroup> matchlist;
+    vector<CardGroup> candidates;
+    pickRun(hand, matchlist, candidates);
+    pickHighestMeld(matchlist, candidates);
+
+    return 0;
+}
+
+int Mgr::pickRun(const map<int, PCard> &hand, vector<CardGroup> &matchlist, vector<CardGroup> &candidates)
+{
+    matchlist.clear();
+    candidates.clear();
+    std::vector<CardGroup> suitlist;
+    suitlist.resize(c_suit);
+    candidates.resize(c_suit);
+    std::for_each(hand.begin(), hand.end(),
+                  [&suitlist, &candidates, this](std::pair<const int, PCard> item) {
+                      if (item.second->rank_ == special_)
+                      {
+                          candidates[e_joker].cardlist_.push_back(item.second);
+                      }
+                      else
+                      {
+                          suitlist[item.second->suit_].cardlist_.push_back(item.second);
+                      }
+                  });
+
+    std::for_each(suitlist.begin(), suitlist.end(),
+                  [&matchlist, &candidates](CardGroup &group) {
+                      std::sort(group.cardlist_.begin(), group.cardlist_.end(), [](const PCard &a, const PCard &b) { return a->rank_ > b->rank_; });
+                      pickRunFromGroup(group, matchlist, candidates);
+                  });
+
+    cout << "同花组" << endl;
+    std::for_each(matchlist.begin(), matchlist.end(), Mgr::printCardGroup);
+
+    return 0;
+}
+
+int Mgr::pickRunFromGroup(const CardGroup &group, vector<CardGroup> &matchlist, vector<CardGroup> &candidates)
+{
+    int matched = 0;
+    CardGroup groupCopy(group);
+    int groupSize = groupCopy.cardlist_.size();
+    if (groupSize < c_minimum && groupSize > 0)
+    {
+        int idx = groupCopy.cardlist_[0]->suit_;
+        assert(idx >= 0 && idx <= 4);
+        candidates[idx].cardlist_.insert(candidates[idx].cardlist_.end(),
+                                         groupCopy.cardlist_.begin(), groupCopy.cardlist_.end());
+        return matched;
+    }
+
+    CardGroup tmpMatched;
+    auto it = groupCopy.cardlist_.begin();
+    tmpMatched.cardlist_.push_back(*it);
+    PCard last = NULL;
+    for (; it < groupCopy.cardlist_.end(); ++it)
+    {
+        if (last == NULL)
+        {
+            last = *it;
+            // cout << "匹配开始" << endl;
+            // cout << "last: " << "NULL" << ":" << "NULL";
+            // cout << " it: " << (*it)->suit_ << ":" << (*it)->rank_ << endl;
+            continue;
+        }
+
+        if ((*it)->rank_ == last->rank_)
+        {
+            // cout << "匹配重复" << endl;
+            // cout << "last: " << last->suit_ << ":" << last->rank_;
+            // cout << " it: " << (*it)->suit_ << ":" << (*it)->rank_ << endl;
+            continue;
+        }
+        else if (last->rank_ - (*it)->rank_ == 1)
+        {
+            // cout << "匹配成功" << endl;
+            // cout << "last: " << last->suit_ << ":" << last->rank_;
+            // cout << " it: " << (*it)->suit_ << ":" << (*it)->rank_ << endl;
+            tmpMatched.cardlist_.push_back(*it);
+            last = *it;
+        }
+        else
+        {
+            // cout << "匹配失败" << endl;
+            // cout << "last: " << last->suit_ << ":" << last->rank_;
+            // cout << " it: " << (*it)->suit_ << ":" << (*it)->rank_ << endl;
+            tmpMatched.reset();
+            tmpMatched.cardlist_.push_back(*it);
+            last = *it;
+            continue;
+        }
+
+        if (tmpMatched.cardlist_.size() == c_minimum - 1)
+        {
+            if ((*(tmpMatched.cardlist_.begin()))->rank_ == 13 && (*(tmpMatched.cardlist_.begin() + 1))->rank_ == 12)
+            {
+                auto ace = std::find_if(groupCopy.cardlist_.begin(), groupCopy.cardlist_.end(), [](const PCard &card) { return card->rank_ == 1; });
+                if (ace != groupCopy.cardlist_.end())
+                {
+                    matched++;
+                    tmpMatched.cardlist_.insert(tmpMatched.cardlist_.begin(), (*ace));
+                    matchlist.push_back(tmpMatched);
+                    groupCopy.removeGroup(tmpMatched);
+                    tmpMatched.reset();
+                    it = groupCopy.cardlist_.begin();
+                    if (it != groupCopy.cardlist_.end())
+                    {
+                        tmpMatched.cardlist_.push_back(*(it));
+                        last = *it;
+                    }
+                }
+            }
+        }
+        else if (tmpMatched.cardlist_.size() == c_minimum)
+        {
+            matched++;
+            matchlist.push_back(tmpMatched);
+            groupCopy.removeGroup(tmpMatched);
+            tmpMatched.reset();
+            it = groupCopy.cardlist_.begin();
+            if (it != groupCopy.cardlist_.end())
+            {
+                tmpMatched.cardlist_.push_back(*it);
+            }
+            last = *it;
+        }
+    }
+
+    if (tmpMatched.cardlist_.size() > 0)
+    {
+        int idx = tmpMatched.cardlist_[0]->suit_;
+        candidates[idx].cardlist_.insert(candidates[idx].cardlist_.end(),
+                                         groupCopy.cardlist_.begin(), groupCopy.cardlist_.end());
+    }
+    return matched;
+}
+
 int Mgr::pickHighestMeld(std::vector<CardGroup> &matchlist, std::vector<CardGroup> &candidates)
 {
+    std::for_each(candidates.begin(), candidates.end(), [](CardGroup &group) {
+        std::sort(group.cardlist_.begin(), group.cardlist_.end(), [](const PCard &a, const PCard &b) {
+            return a->rank_ > b->rank_;
+        });
+    });
 
+    cout << "候选组选拔" << endl;
+    std::for_each(candidates.begin(), candidates.end(), Mgr::printCardGroup);
+
+    int targetGoal[c_suit] = {};
+    for (int i = 1; i < c_suit; i++)
+    {
+        targetGoal[i] = 0;
+    }
 
     return 0;
 }
